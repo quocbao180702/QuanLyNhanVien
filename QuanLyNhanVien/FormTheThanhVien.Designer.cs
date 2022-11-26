d8bb35a81e807b3326371cee5b90bd2795df4355
Giang Khuu <giangkhuu3897@gmail.com>
2022-11-26 13:38:11 +0700

    sửa from Tạo Thẻ Thành viên

--- QuanLyNhanVien/FormTheThanhVien.Designer.cs
+++ QuanLyNhanVien/FormTheThanhVien.Designer.cs
@@ -35,17 +35,18 @@
             this.btnThoat = new System.Windows.Forms.Button();
             this.txtTenKH = new System.Windows.Forms.TextBox();
             this.txtSDT = new System.Windows.Forms.TextBox();
-            this.txtDate = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.datNgay = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
             // 
             // lbTenKhachHang
             // 
             this.lbTenKhachHang.AutoSize = true;
             this.lbTenKhachHang.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbTenKhachHang.Location = new System.Drawing.Point(173, 114);
+            this.lbTenKhachHang.Location = new System.Drawing.Point(231, 140);
+            this.lbTenKhachHang.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbTenKhachHang.Name = "lbTenKhachHang";
-            this.lbTenKhachHang.Size = new System.Drawing.Size(115, 19);
+            this.lbTenKhachHang.Size = new System.Drawing.Size(147, 22);
             this.lbTenKhachHang.TabIndex = 0;
             this.lbTenKhachHang.Text = "Tên Khách Hàng:";
             // 
@@ -53,9 +54,10 @@
             // 
             this.lbSDT.AutoSize = true;
             this.lbSDT.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbSDT.Location = new System.Drawing.Point(173, 161);
+            this.lbSDT.Location = new System.Drawing.Point(231, 198);
+            this.lbSDT.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbSDT.Name = "lbSDT";
-            this.lbSDT.Size = new System.Drawing.Size(40, 19);
+            this.lbSDT.Size = new System.Drawing.Size(52, 22);
             this.lbSDT.TabIndex = 1;
             this.lbSDT.Text = "SĐT:";
             // 
@@ -63,18 +65,20 @@
             // 
             this.lbDate.AutoSize = true;
             this.lbDate.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbDate.Location = new System.Drawing.Point(173, 202);
+            this.lbDate.Location = new System.Drawing.Point(231, 249);
+            this.lbDate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbDate.Name = "lbDate";
-            this.lbDate.Size = new System.Drawing.Size(41, 19);
+            this.lbDate.Size = new System.Drawing.Size(53, 22);
             this.lbDate.TabIndex = 1;
             this.lbDate.Text = "Date:";
             // 
             // btnDangKi
             // 
             this.btnDangKi.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDangKi.Location = new System.Drawing.Point(242, 273);
+            this.btnDangKi.Location = new System.Drawing.Point(323, 336);
+            this.btnDangKi.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnDangKi.Name = "btnDangKi";
-            this.btnDangKi.Size = new System.Drawing.Size(94, 35);
+            this.btnDangKi.Size = new System.Drawing.Size(125, 43);
             this.btnDangKi.TabIndex = 2;
             this.btnDangKi.Text = "Đăng Kí";
             this.btnDangKi.UseVisualStyleBackColor = true;
@@ -82,52 +86,57 @@
             // btnThoat
             // 
             this.btnThoat.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnThoat.Location = new System.Drawing.Point(480, 273);
+            this.btnThoat.Location = new System.Drawing.Point(640, 336);
+            this.btnThoat.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnThoat.Name = "btnThoat";
-            this.btnThoat.Size = new System.Drawing.Size(94, 35);
+            this.btnThoat.Size = new System.Drawing.Size(125, 43);
             this.btnThoat.TabIndex = 2;
             this.btnThoat.Text = "Thoát";
             this.btnThoat.UseVisualStyleBackColor = true;
             // 
             // txtTenKH
             // 
-            this.txtTenKH.Location = new System.Drawing.Point(302, 115);
+            this.txtTenKH.Location = new System.Drawing.Point(403, 142);
+            this.txtTenKH.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtTenKH.Name = "txtTenKH";
-            this.txtTenKH.Size = new System.Drawing.Size(272, 20);
+            this.txtTenKH.Size = new System.Drawing.Size(361, 22);
             this.txtTenKH.TabIndex = 3;
             // 
             // txtSDT
             // 
-            this.txtSDT.Location = new System.Drawing.Point(302, 160);
+            this.txtSDT.Location = new System.Drawing.Point(403, 197);
+            this.txtSDT.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtSDT.Name = "txtSDT";
-            this.txtSDT.Size = new System.Drawing.Size(272, 20);
+            this.txtSDT.Size = new System.Drawing.Size(361, 22);
             this.txtSDT.TabIndex = 3;
-            // 
-            // txtDate
-            // 
-            this.txtDate.Location = new System.Drawing.Point(302, 203);
-            this.txtDate.Name = "txtDate";
-            this.txtDate.Size = new System.Drawing.Size(272, 20);
-            this.txtDate.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.label1.Location = new System.Drawing.Point(298, 40);
+            this.label1.Location = new System.Drawing.Point(397, 49);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(190, 31);
+            this.label1.Size = new System.Drawing.Size(231, 39);
             this.label1.TabIndex = 4;
             this.label1.Text = "Thẻ Thành Viên";
             // 
-            // TaoTheThanhVien
+            // datNgay
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.datNgay.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.datNgay.Location = new System.Drawing.Point(404, 249);
+            this.datNgay.Name = "datNgay";
+            this.datNgay.Size = new System.Drawing.Size(361, 22);
+            this.datNgay.TabIndex = 5;
+            // 
+            // FormTheThanhVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.datNgay);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtDate);
             this.Controls.Add(this.txtSDT);
             this.Controls.Add(this.txtTenKH);
             this.Controls.Add(this.btnThoat);
@@ -135,7 +144,8 @@
             this.Controls.Add(this.lbDate);
             this.Controls.Add(this.lbSDT);
             this.Controls.Add(this.lbTenKhachHang);
-            this.Name = "TaoTheThanhVien";
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "FormTheThanhVien";
             this.Text = "TaoTheThanhVien";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -151,7 +161,7 @@
         private System.Windows.Forms.Button btnThoat;
         private System.Windows.Forms.TextBox txtTenKH;
         private System.Windows.Forms.TextBox txtSDT;
-        private System.Windows.Forms.TextBox txtDate;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker datNgay;
     }
 }
