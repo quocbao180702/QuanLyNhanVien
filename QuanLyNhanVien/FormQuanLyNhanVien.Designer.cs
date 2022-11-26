e3dc69c22ab8812055f45fd4cb10e108abe75aa4
quocbao <quocbao@BAODANG>
2022-11-26 12:53:03 +0700

    Quan lý Nhan Vien

---
+++ QuanLyNhanVien/FormQuanLyNhanVien.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace QuanLyNhanVien
+{
+    partial class FormQuanLyNhanVien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnQuanLy = new System.Windows.Forms.Button();
+            this.btnHuy = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.btnQuayLai = new System.Windows.Forms.Button();
+            this.grbLuaChon = new System.Windows.Forms.GroupBox();
+            this.radQuanLyKho = new System.Windows.Forms.RadioButton();
+            this.radBanHang = new System.Windows.Forms.RadioButton();
+            this.grbLuaChon.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.Blue;
+            this.lblTitle.Location = new System.Drawing.Point(198, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(442, 45);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "QUẢN LÝ NHÂN VIÊN";
+            // 
+            // btnQuanLy
+            // 
+            this.btnQuanLy.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuanLy.Location = new System.Drawing.Point(57, 422);
+            this.btnQuanLy.Name = "btnQuanLy";
+            this.btnQuanLy.Size = new System.Drawing.Size(129, 61);
+            this.btnQuanLy.TabIndex = 3;
+            this.btnQuanLy.Text = "Quản Lý";
+            this.btnQuanLy.UseVisualStyleBackColor = true;
+            // 
+            // btnHuy
+            // 
+            this.btnHuy.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHuy.Location = new System.Drawing.Point(257, 422);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(129, 61);
+            this.btnHuy.TabIndex = 3;
+            this.btnHuy.Text = "Hủy Chọn";
+            this.btnHuy.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThoat.Location = new System.Drawing.Point(474, 422);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(129, 61);
+            this.btnThoat.TabIndex = 3;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // btnQuayLai
+            // 
+            this.btnQuayLai.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuayLai.Location = new System.Drawing.Point(687, 422);
+            this.btnQuayLai.Name = "btnQuayLai";
+            this.btnQuayLai.Size = new System.Drawing.Size(129, 61);
+            this.btnQuayLai.TabIndex = 3;
+            this.btnQuayLai.Text = "Quay Lại";
+            this.btnQuayLai.UseVisualStyleBackColor = true;
+            // 
+            // grbLuaChon
+            // 
+            this.grbLuaChon.Controls.Add(this.radBanHang);
+            this.grbLuaChon.Controls.Add(this.radQuanLyKho);
+            this.grbLuaChon.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grbLuaChon.Location = new System.Drawing.Point(108, 71);
+            this.grbLuaChon.Name = "grbLuaChon";
+            this.grbLuaChon.Size = new System.Drawing.Size(681, 291);
+            this.grbLuaChon.TabIndex = 4;
+            this.grbLuaChon.TabStop = false;
+            this.grbLuaChon.Text = "Lựa chọn";
+            // 
+            // radQuanLyKho
+            // 
+            this.radQuanLyKho.AutoSize = true;
+            this.radQuanLyKho.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radQuanLyKho.Location = new System.Drawing.Point(64, 69);
+            this.radQuanLyKho.Name = "radQuanLyKho";
+            this.radQuanLyKho.Size = new System.Drawing.Size(312, 37);
+            this.radQuanLyKho.TabIndex = 0;
+            this.radQuanLyKho.TabStop = true;
+            this.radQuanLyKho.Text = "Quản Lý Nhân Viên Kho";
+            this.radQuanLyKho.UseVisualStyleBackColor = true;
+            // 
+            // radBanHang
+            // 
+            this.radBanHang.AutoSize = true;
+            this.radBanHang.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radBanHang.Location = new System.Drawing.Point(64, 197);
+            this.radBanHang.Name = "radBanHang";
+            this.radBanHang.Size = new System.Drawing.Size(375, 37);
+            this.radBanHang.TabIndex = 0;
+            this.radBanHang.TabStop = true;
+            this.radBanHang.Text = "Quản Lý Nhân Viên Bán Hàng";
+            this.radBanHang.UseVisualStyleBackColor = true;
+            // 
+            // FormQuanLyNhanVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(871, 557);
+            this.Controls.Add(this.grbLuaChon);
+            this.Controls.Add(this.btnQuayLai);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnHuy);
+            this.Controls.Add(this.btnQuanLy);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "FormQuanLyNhanVien";
+            this.Text = "FormQuanLyNhanVien";
+            this.grbLuaChon.ResumeLayout(false);
+            this.grbLuaChon.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnQuanLy;
+        private System.Windows.Forms.Button btnHuy;
+        private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.Button btnQuayLai;
+        private System.Windows.Forms.GroupBox grbLuaChon;
+        private System.Windows.Forms.RadioButton radBanHang;
+        private System.Windows.Forms.RadioButton radQuanLyKho;
+    }
+}
